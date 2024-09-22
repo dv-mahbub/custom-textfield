a6a93845c7645c8a097075eee8b2fbdd0e0b3ac1
dv-mahbub <dv.mahbub@gmail.com>
2024-09-22 22:19:55 +0600

    additional parameters

--- lib/custom_text_field.dart
+++ lib/custom_text_field.dart
@@ -1,6 +1,7 @@
+import 'package:custom_textfield/colors.dart';
 import 'package:flutter/material.dart';
 
-class CustomTextField extends StatelessWidget {
+class CustomTextField extends StatefulWidget {
   final TextEditingController controller;
   final String? hint;
   final double? height;
@@ -8,6 +9,15 @@ class CustomTextField extends StatelessWidget {
   final String? label;
   final Color? backgroundColor;
   final String? Function(String? value)? validator;
+  final Widget? suffix;
+  final Widget? prefix;
+  final bool? obscureText;
+  final TextInputType? keyboardType;
+  final bool enabled;
+  final int? maxLine;
+  final int? minLine;
+  final FocusNode? focusNode;
+  final void Function(String value)? onChange;
 
   const CustomTextField({
     super.key,
@@ -18,17 +28,34 @@ class CustomTextField extends StatelessWidget {
     this.height,
     this.backgroundColor,
     this.validator,
+    this.suffix,
+    this.prefix,
+    this.obscureText,
+    this.keyboardType,
+    this.enabled = true,
+    this.maxLine,
+    this.minLine,
+    this.focusNode,
+    this.onChange,
   });
 
+  @override
+  State<CustomTextField> createState() => _CustomTextFieldState();
+}
+
+class _CustomTextFieldState extends State<CustomTextField> {
+  double borrderRadius = 8.0;
+  bool showText = false;
   @override
   Widget build(BuildContext context) {
     double screenWidth = MediaQuery.of(context).size.width;
 
     return Container(
-      width: width ?? (screenWidth < 600 ? screenWidth * .9 : screenWidth * .5),
+      width: widget.width ??
+          (screenWidth < 600 ? screenWidth * .9 : screenWidth * .5),
       decoration: BoxDecoration(
         color: Colors.white,
-        borderRadius: BorderRadius.circular(8.0),
+        borderRadius: BorderRadius.circular(borrderRadius),
         boxShadow: [
           BoxShadow(
             color: Colors.grey.withOpacity(0.5),
@@ -39,19 +66,56 @@ class CustomTextField extends StatelessWidget {
         ],
       ),
       child: TextFormField(
-          controller: controller,
-          decoration: InputDecoration(
-            fillColor: backgroundColor ?? Colors.white,
-            filled: true,
-            border: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(8.0),
-            ),
-            contentPadding:
-                const EdgeInsets.symmetric(horizontal: 12.0, vertical: 8.0),
-            hintText: hint ?? 'Enter text',
-            label: label != null ? Text(label!) : null,
+        controller: widget.controller,
+        obscureText: widget.obscureText! && !showText,
+        maxLines: widget.maxLine,
+        minLines: widget.minLine,
+        focusNode: widget.focusNode,
+        onChanged: widget.onChange,
+        decoration: InputDecoration(
+          enabled: widget.enabled,
+          suffix: widget.suffix,
+          prefix: widget.prefix,
+          suffixIcon: widget.obscureText!
+              ? IconButton(
+                  icon:
+                      Icon(showText ? Icons.visibility : Icons.visibility_off),
+                  onPressed: () {
+                    setState(() {
+                      showText = !showText;
+                    });
+                  },
+                )
+              : null,
+          fillColor: widget.backgroundColor ?? Colors.white,
+          filled: true,
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(borrderRadius),
+            borderSide: BorderSide(
+                color: AppColors.primaryColor), // Default border color
           ),
-          validator: validator),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(borrderRadius),
+            borderSide: BorderSide(
+                color: Colors.green, width: 1.0), // Focused border color
+          ),
+          errorBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(borrderRadius),
+            borderSide:
+                BorderSide(color: Colors.red, width: 1.0), // Error border color
+          ),
+          focusedErrorBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(borrderRadius),
+            borderSide: BorderSide(
+                color: Colors.redAccent, width: 1.0), // Focused error border
+          ),
+          contentPadding:
+              const EdgeInsets.symmetric(horizontal: 12.0, vertical: 8.0),
+          hintText: widget.hint ?? 'Enter text',
+          label: widget.label != null ? Text(widget.label!) : null,
+        ),
+        validator: widget.validator,
+      ),
     );
   }
 }
